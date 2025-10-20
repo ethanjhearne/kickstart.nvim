5038082dcbd254cc731e18871689a3899bd4c97d
Hearne, Ethan <ethan.hearne@viasat.com>
2025-10-20 13:07:06 -0400

    Fix tmux commands printing in terminal mode

--- lua/plugins/tmux-navigator.lua
+++ lua/plugins/tmux-navigator.lua
@@ -1,5 +1,6 @@
 return {
   'christoomey/vim-tmux-navigator',
+  lazy=false,
   cmd = {
     'TmuxNavigateLeft',
     'TmuxNavigateDown',
@@ -8,14 +9,17 @@ return {
     'TmuxNavigatePrevious',
     'TmuxNavigatorProcessList',
   },
-  keys = {
-    { '<C-h>', '<CMD><C-U>TmuxNavigateLeft<CR>' },
-    { '<C-j>', '<CMD><C-U>TmuxNavigateDown<CR>' },
-    { '<C-k>', '<CMD><C-U>TmuxNavigateUp<CR>' },
-    { '<C-l>', '<CMD><C-U>TmuxNavigateRight<CR>' },
-    { '<C-\\>', '<CMD><C-U>TmuxNavigatePrevious<CR>' },
-  },
   config = function()
     vim.g.tmux_navigator_no_wrap = true
+
+    vim.keymap.set('t', '<C-h>', '<C-\\><C-N><CMD>TmuxNavigateLeft<CR>', { desc = 'Move focus to the left window' })
+    vim.keymap.set('t', '<C-l>', '<C-\\><C-N><CMD>TmuxNavigateRight<CR>', { desc = 'Move focus to the right window' })
+    vim.keymap.set('t', '<C-j>', '<C-\\><C-N><CMD>TmuxNavigateDown<CR>', { desc = 'Move focus to the lower window' })
+    vim.keymap.set('t', '<C-k>', '<C-\\><C-N><CMD>TmuxNavigateUp<CR>', { desc = 'Move focus to the upper window' })
+
+    vim.keymap.set('n', '<C-h>', '<CMD>TmuxNavigateLeft<CR>', { desc = 'Move focus to the left window' })
+    vim.keymap.set('n', '<C-l>', '<CMD>TmuxNavigateRight<CR>', { desc = 'Move focus to the right window' })
+    vim.keymap.set('n', '<C-j>', '<CMD>TmuxNavigateDown<CR>', { desc = 'Move focus to the lower window' })
+    vim.keymap.set('n', '<C-k>', '<CMD>TmuxNavigateUp<CR>', { desc = 'Move focus to the upper window' })
   end,
 }
